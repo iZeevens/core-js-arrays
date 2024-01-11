03a9c4901dfd0424c43f1c2ce97d479e46ffb7b1
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-01-11 23:20:29 +0600

    feat: add getFalsyValuesCount

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -393,8 +393,16 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  return arr.reduce((accum, item) => {
+    if (!item) {
+      return accum + 1;
+    }
+    return accum;
+  }, 0);
 }
 
 /**
