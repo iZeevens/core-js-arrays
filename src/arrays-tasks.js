c00b2a357e2c7a5b154aa99b1dd6b4fd6ce2396d
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-01-12 00:28:37 +0600

    feat: add getIdentityMatrix

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -423,8 +423,12 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (_, firstIndex) =>
+    Array.from({ length: n }, (i, secondIndex) =>
+      firstIndex === secondIndex ? 1 : 0
+    )
+  );
 }
 
 /**
