b843d96966a515012aeee6900768c4dd9237f43b
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-01-12 00:43:18 +0600

    feat: add getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -448,8 +448,10 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return Array.from(numbers, (item, index) =>
+    item % 2 === 1 ? index : -1
+  ).filter((item) => item !== -1);
 }
 
 /**
