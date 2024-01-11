8d768f513127d3a32ea35bcfc835b0a26e35ef34
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-01-12 01:36:12 +0600

    feat: add findCommonElements

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -506,8 +506,13 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  return arr1.reduce((accum, item) => {
+    if (arr2.includes(item)) {
+      accum.push(item);
+    }
+    return accum;
+  }, []);
 }
 
 /**
