7ce67d98ee0391f5515803ed783b4f747614a3af
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-01-11 22:17:14 +0600

    feat: add generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -354,8 +354,9 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const array = Array(len);
+  return Array.from(array, (_, index) => index * 2 + 1);
 }
 
 /**
